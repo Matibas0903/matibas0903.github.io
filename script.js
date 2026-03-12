46233cb9c5504ea40ce3ff07948aac0a15b49763
Matias Basterra <basterramatias2020@gmail.com>
2026-03-12 12:03:01 -0300

    fix script

--- script.js
+++ script.js
@@ -18,32 +18,35 @@ function verMas() {
 
 
     
-   // Download
-  const modalDownload = document.getElementById("modalDownload");
-  if(download && download.trim() !== ""){
-      modalDownload.href = download;
-      modalDownload.download = download;
-      modalDownload.classList.remove("d-none");
-  } else {
-      modalDownload.classList.add("d-none");
-  }
+// Visit
+const modalVisit = document.getElementById("modalVisit");
+if(visit && visit.trim() !== ""){
+    visit = visit.replace(/\\/g, "/"); // <- reemplaza \ por /
+    modalVisit.href = visit;
+    modalVisit.classList.remove("d-none");
+} else {
+    modalVisit.classList.add("d-none");
+}
 
-  // Visit
-  const modalVisit = document.getElementById("modalVisit");
-  if(visit && visit.trim() !== ""){
-      modalVisit.href = visit;
-      modalVisit.classList.remove("d-none");
-  } else {
-      modalVisit.classList.add("d-none");
-  }
-  // Repo
-  const modalRepo =   document.getElementById("modalRepo");
-  if(repo && repo.trim() !== ""){
-      modalRepo.href = repo;
-      modalRepo.classList.remove("d-none");
-  } else {
-      modalRepo.classList.add("d-none");
-  }
+// Download
+const modalDownload = document.getElementById("modalDownload");
+if(download && download.trim() !== ""){
+    download = download.replace(/\\/g, "/"); // <- reemplaza \ por /
+    modalDownload.href = download;
+    modalDownload.download = download;
+    modalDownload.classList.remove("d-none");
+} else {
+    modalDownload.classList.add("d-none");
+}
+// Repo
+const modalRepo = document.getElementById("modalRepo");
+if(repo && repo.trim() !== ""){
+    repo = repo.replace(/\\/g, "/"); // reemplaza \ por /
+    modalRepo.href = repo;
+    modalRepo.classList.remove("d-none");
+} else {
+    modalRepo.classList.add("d-none");
+}
 
   // Elementos
     document.getElementById("modalTitle").textContent = title;
